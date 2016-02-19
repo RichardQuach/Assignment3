7fb562b67e37a02ff10568cc1354fecc88a11229
RichardQuach <rquach1@asu.edu>
2016-02-19 11:12:25 -0700

    Initial Version (Part 1)

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,67 @@
+
+package cse360assign3;
+
+/**
+ * 
+ * @author Richard Quach
+ * @version February 19, 2016
+ * pin: 712
+ * 
+ * Calculator performs mathematical operations to a total.
+ */
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * gets the total value
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds a given value to the total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts a given value from the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiples a given value to the total
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides a given value from the total
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * returns a string of the history of calculations performed
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
